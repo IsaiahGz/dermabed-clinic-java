c913d3d1ea8d038b36de927e503e78d0f408ee90
IsaiahGz <isaiahgala99@gmail.com>
2023-07-30 14:41:14 -0400

    Remove unused imports

--- discovery-server/src/main/java/com/isaiahg/discoveryserver/SecurityConfig.java
+++ discovery-server/src/main/java/com/isaiahg/discoveryserver/SecurityConfig.java
@@ -2,10 +2,7 @@ package com.isaiahg.discoveryserver;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
